910391cdcf612cf25c3a3b36fea9969530f8bcef
JosephZegarra <u18215722@utp.edu.pe>
2023-09-15 12:26:41 -0500

    Implementacion del metodo CopiarContenidoEImpresion en la clase Detalles_Pedidos,ademas de su instanciacion y uso en el main ImportadorCSV

---
+++ src/main/java/Datos/Detalles_Pedidos.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Datos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Home
+ */
+public class Detalles_Pedidos 
+        
+{
+    
+    private String PrecioUnidad;
+    private String Cantidad;
+    private String Descuento;
+
+    public Detalles_Pedidos() 
+    {
+
+    }
+
+    // Getters y setters para los atributos
+
+    public String getPrecioUnidad() {
+        return PrecioUnidad;
+    }
+
+    public void setPrecioUnidad(String PrecioUnidad) {
+        this.PrecioUnidad = PrecioUnidad;
+    }
+
+    public String getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(String Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+
+    public String getDescuento() {
+        return Descuento;
+    }
+
+    public void setDescuento(String Descuento) {
+        this.Descuento = Descuento;
+    }
+
+   
+
+    @Override
+    public String toString() {
+        return PrecioUnidad + "; " + Cantidad + "; " + Descuento;
+    }
+    
+    
+    String line;
+    String csvDelimitador = ";"; // El delimitador que separa los valores en el CSV
+    
+    
+    public void CopiarContenidoEImpresion(String csvFile, int numFilas)
+    {  
+        Detalles_Pedidos[] listaobjetos = new Detalles_Pedidos[numFilas];
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) 
+            {
+                int indice = 0;
+                
+                while ((line = br.readLine()) != null) {
+                    // Dividir la línea en partes usando el delimitador
+                    String[] partes = line.split(csvDelimitador);
+
+                            Detalles_Pedidos objeto=new Detalles_Pedidos();
+                            objeto.setPrecioUnidad(partes[0]);
+                            objeto.setCantidad(partes[1]);
+                            objeto.setDescuento(partes[2]);
+                            
+
+                    // Agregar el objeto al arreglo
+                    listaobjetos[indice] = objeto;
+                    indice++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            // Ahora, objetos contiene todos los objetos creados a partir del archivo CSV
+            for (Detalles_Pedidos objeto : listaobjetos) 
+            {
+                System.out.println(objeto);
+            }
+    }
+    
+}
--- src/main/java/Datos/ImportadorCSV.java
+++ src/main/java/Datos/ImportadorCSV.java
@@ -8,6 +8,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import Datos.*;
 /**
  *
  * @author Home
@@ -22,18 +23,17 @@ public class ImportadorCSV
     {   
         
             
-        String csvFile = "C:\\Users\\Home\\Downloads\\Categorias.csv"; // Reemplaza con la ruta de tu archivo CSV
+        
         
         String line;
-        String csvDelimiter = ";"; // El delimitador que separa los valores en el CSV
+        String csvDelimitador = ";"; // El delimitador que separa los valores en el CSV
         
-        
-        
+
         Scanner consola = new Scanner(System.in);
         
         
       //interface
-       System.out.println("Seleccione una opción:");
+        System.out.println("Seleccione una opción:");
             System.out.println("1. Categoría");
             System.out.println("2. Artículos");
             System.out.println("3. Proveedores");
@@ -42,27 +42,109 @@ public class ImportadorCSV
             System.out.println("6. Detalles Pedidos");
             System.out.println("7. Compañías Envíos");
             
-        int NumFormulario = consola.nextInt();
+        int numDirectorio = consola.nextInt();
+        String csvFile =SeleccionDirectorio(numDirectorio);  // Reemplaza con la ruta de tu archivo CSV
         //ingreso de tipo de formulario al contarFilasEnCSV     
-        int numFilas = contarFilasEnCSV(NumFormulario);//1: tipo de formulario que se va a contar
-        ObjetoCSV[] objetos = new ObjetoCSV[numFilas]; 
+        int numFilas = contarFilasEnCSV(csvFile);//1: tipo de formulario que se va a contar
+        
+        
+        switch(numDirectorio) 
+        {
+            case 1: //Instancia del primer objeto
+                
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+               
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            case 6:
+                Detalles_Pedidos objDetalles_Pedidos= new Detalles_Pedidos();
+                objDetalles_Pedidos.CopiarContenidoEImpresion(csvFile, numFilas);
+                break;
+            case 7:
+                
+                break;
+            default:
+                System.out.println("no coincide con ninguno de los casos anteriores");
+                break;
+        }
         
         
         
-
+        
+       // ObjetoCSV[] objetos = new ObjetoCSV[numFilas]; //en este arreglo de objetos se guardaran los objetos(fila de datos)que se lean del archivo csv 
+        
+        
+        
+        
+        
+        
+        
+/*
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) 
         {
             int indice = 0;
             while ((line = br.readLine()) != null) {
                 // Dividir la línea en partes usando el delimitador
-                String[] partes = line.split(csvDelimiter);
+                String[] partes = line.split(csvDelimitador);
+                
 
                 // Crear un objeto y asignar valores a sus atributos
+               /* 
+
                 ObjetoCSV objeto = new ObjetoCSV();
                 objeto.setIdCategoria(partes[0]);
                 objeto.setNombreCategoria(partes[1]);
-                objeto.setDescripcionCategoria(partes[2]);
+                objeto.setDescripcionCategoria(partes[2]); 
                 
+               
+                //case
+                switch (numDirectorio) 
+                {
+                    case 1:
+                        ObjetoCSV objeto = new ObjetoCSV();
+                        objeto.setIdCategoria(partes[0]);
+                        objeto.setNombreCategoria(partes[1]);
+                        objeto.setDescripcionCategoria(partes[2]);
+                        break;
+                    case 2:
+
+                        DatosCSVDetallesPedido objeto=new DatosCSVDetallesPedido();
+                        objeto.setIdPedido(partes[0]);
+                        objeto.setProducto(partes[1]);
+                        objeto.setPrecioUnidad(partes[2]);
+                        objeto.setCantidad(partes[3]);
+                        objeto.setDescuento(partes[4]);
+
+                    case 3:
+                        // Código para la opción 3
+                        break;
+                    case 4:
+                        // Código para la opción 4
+                        break;
+                    case 5:
+                        // Código para la opción 5
+                        break;
+                    case 6:
+                        // Código para la opción 6
+                        break;
+                    case 7:
+                        // Código para la opción 7
+                        break;
+                    default:
+                        // Código para el caso por defecto (si no coincide con ninguno de los casos anteriores)
+                        break;
+                }
+        
 
                 // Agregar el objeto al arreglo
                 objetos[indice] = objeto;
@@ -72,17 +154,12 @@ public class ImportadorCSV
             e.printStackTrace();
         }
         
-        
-        
-        
-        
-        
 
         // Ahora, objetos contiene todos los objetos creados a partir del archivo CSV
         for (ObjetoCSV objeto : objetos) {
             System.out.println(objeto);
         }
-        
+ */       
         
         
         
@@ -103,13 +180,54 @@ public class ImportadorCSV
         
     }
     
+    //Metodo para elegir Directorio del archivo
+    public static String SeleccionDirectorio(int numDirectorio)
+    {   String CaseDirectorio="";
+        switch(numDirectorio) 
+        {
+            case 1:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Categorias.csv";
+                break;
+            case 2:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Articulos.csv";
+                break;
+            case 3:
+               CaseDirectorio="C:\\Users\\Home\\Downloads\\Proveedores.csv";
+                break;
+            case 4:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Clientes.csv";
+                break;
+            case 5:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Pedidos.csv";
+                break;
+            case 6:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Detalles_Pedidos.csv";
+                break;
+            case 7:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Compañias Envíos.csv";
+                break;
+            default:
+                System.out.println("no coincide con ninguno de los casos anteriores");
+                break;
+        }
+        
+                
+        return CaseDirectorio;
+    }
+    
+    
+    
+    
+    
     
     
     //metodos para contar filas de un archivo
     
-    public static int contarFilasEnCSV(int numDatosFormulario) 
+    public static int contarFilasEnCSV(String csvFile) 
     {
         int contador = 0;
+        
+        /*
         String CaseDirectorio="";
         
         switch (numDatosFormulario) {
@@ -117,7 +235,7 @@ public class ImportadorCSV
                 CaseDirectorio="C:\\Users\\Home\\Downloads\\Categorias.csv";
                 break;
             case 2:
-                CaseDirectorio="C:\\Users\\Home\\Downloads\\Articulos.csv";
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Detalles_Pedidos.csv";
                 break;
             case 3:
                CaseDirectorio="C:\\Users\\Home\\Downloads\\Proveedores.csv";
@@ -140,8 +258,10 @@ public class ImportadorCSV
         }
         
 
+        */
+        
        
-        try (BufferedReader br = new BufferedReader(new FileReader(CaseDirectorio))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while (br.readLine() != null) {
                 contador++;
             }
@@ -152,7 +272,59 @@ public class ImportadorCSV
         
         
         return contador;
+        
     }
+    
+    
+    /*
+    //metodo que cree objetos segun el directorio seleccionado
+    public static objeto CreadorDeObjetosVarios(int numDirectorio, String[] partes)
+    {   
+        
+        switch(numDirectorio) 
+        {
+            case 1:
+                        ObjetoCSV objeto = new ObjetoCSV();
+                        objeto.setIdCategoria(partes[0]);
+                        objeto.setNombreCategoria(partes[1]);
+                        objeto.setDescripcionCategoria(partes[2]);
+                        return objeto;
+                        break;
+                    case 2:
+
+                        DatosCSVDetallesPedido objeto=new DatosCSVDetallesPedido();
+                        objeto.setIdPedido(partes[0]);
+                        objeto.setProducto(partes[1]);
+                        objeto.setPrecioUnidad(partes[2]);
+                        objeto.setCantidad(partes[3]);
+                        objeto.setDescuento(partes[4]);
+                        return objeto;
+                    break;
+            case 3:
+               CaseDirectorio="C:\\Users\\Home\\Downloads\\Proveedores.csv";
+                break;
+            case 4:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Clientes.csv";
+                break;
+            case 5:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Pedidos.csv";
+                break;
+            case 6:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Detalles Pedidos.csv";
+                break;
+            case 7:
+                CaseDirectorio="C:\\Users\\Home\\Downloads\\Compañias Envíos.csv";
+                break;
+            default:
+                System.out.println("no coincide con ninguno de los casos anteriores");
+                break;
+        }
+        
+                
+        
+    }
+    
+    */
   
     /*
     public static int contarFilasEnCSV(String archivoCSV) {
@@ -223,66 +395,6 @@ class ObjetoCSV
       
     }
 
-
-
-class DatosCSVDetallesPedido {
-    private String IdPedido;
-    private String Producto;
-    private String PrecioUnidad;
-    private String Cantidad;
-    private String Descuento;
-
-    public DatosCSVDetallesPedido() {
-
-    }
-
-    // Getters y setters para los atributos
-
-    public String getIdPedido() {
-        return IdPedido;
-    }
-
-    public void setIdPedido(String IdPedido) {
-        this.IdPedido = IdPedido;
-    }
-
-    public String getProducto() {
-        return Producto;
-    }
-
-    public void setProducto(String Producto) {
-        this.Producto = Producto;
-    }
-
-    public String getPrecioUnidad() {
-        return PrecioUnidad;
-    }
-
-    public void setPrecioUnidad(String PrecioUnidad) {
-        this.PrecioUnidad = PrecioUnidad;
-    }
-
-    public String getCantidad() {
-        return Cantidad;
-    }
-
-    public void setCantidad(String Cantidad) {
-        this.Cantidad = Cantidad;
-    }
-
-    public String getDescuento() {
-        return Descuento;
-    }
-
-    public void setDescuento(String Descuento) {
-        this.Descuento = Descuento;
-    }
-
-    @Override
-    public String toString() {
-        return IdPedido + "; " + Producto + "; " + PrecioUnidad + "; " + Cantidad + "; " + Descuento;
-    }
-}
 
 
 
