c4774a3832b8323a9ed92e1ea25a1ebd74d3a4f9
rtp-robinson <u19220090@utp.edu.pe>
2023-10-17 08:13:20 -0500

    cargar datos de la tabla detalle pedido - FORMALUARIO REGISTRO DE PEDIDOS y DETALLE DE PEDIDOS

--- src/main/java/Ventanas/Registro_de_Pedidos.java
+++ src/main/java/Ventanas/Registro_de_Pedidos.java
@@ -19,10 +19,10 @@ import javax.swing.text.Element;
 public class Registro_de_Pedidos extends javax.swing.JFrame {
 
    DefaultTableModel tablaDetalles;
-    JFileChooser archivo = new JFileChooser(System.getProperty("user.dir"));
+   JFileChooser archivo = new JFileChooser(System.getProperty("user.dir"));
     
-    private void cargarRegistroPedidos()
-    {
+    private void cargarDetallePedidos()
+    { 
         try {
             String texto = "";
             Object cabeceras[] = {"Id. de pedido","Producto","Precio por unidad","Cantidad","Descuento"};
@@ -34,6 +34,7 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
             {
                 FileReader fichero = new FileReader(abrir);
                 BufferedReader leer = new BufferedReader(fichero);
+                
                 while ((texto=leer.readLine()) != null)
                 {
                     String registro[] = texto.split(";");
@@ -56,15 +57,17 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
     {
         try {
             String texto = "";
-            Object cabeceras[] = {"Id. de pedido","Producto","Precio por unidad","Cantidad","Descuento"};
+            Object cabeceras[] = {"Id. de pedido","Cliente","Fecha de pedido","Fecha de entrega","Fecha de envío","Forma de envío","Cargo"};
             tablaDetalles = new DefaultTableModel(cabeceras,0);
             archivo.showOpenDialog(this);
             File abrir = archivo.getSelectedFile();
-            Object[] elemento = new Object[5];
+            Object[] elemento = new Object[7];
             if (abrir != null)
             {
                 FileReader fichero = new FileReader(abrir);
                 BufferedReader leer = new BufferedReader(fichero);
+                
+                
                 while ((texto=leer.readLine()) != null)
                 {
                     String registro[] = texto.split(";");
@@ -73,10 +76,13 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
                     elemento[2] = registro[2];
                     elemento[3] = registro[3];
                     elemento[4] = registro[4];
+                    elemento[5] = registro[5];
+                    elemento[6] = registro[6];
+                    
                     tablaDetalles.addRow(elemento);
                     
                 }
-                jTableDetalPedido.setModel(tablaDetalles);
+                jTablePedido.setModel(tablaDetalles);
             }
         } catch (IOException e) 
         {   System.out.println("Error" + e);
@@ -137,12 +143,14 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
         jLabel31 = new javax.swing.JLabel();
         jLabel32 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTablePedido = new javax.swing.JTable();
         jScrollBar1 = new javax.swing.JScrollBar();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuImportar = new javax.swing.JMenuItem();
         jMenuPrincipal = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuPedidos = new javax.swing.JMenuItem();
+        jMenuImportar = new javax.swing.JMenuItem();
 
         jMenuItem1.setText("jMenuItem1");
 
@@ -239,8 +247,9 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel4Layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jLabel27)
-                .addGap(46, 46, 46)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel28)
                     .addComponent(jTextDetId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -260,12 +269,12 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
                 .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel32)
                     .addComponent(jTextDetDescuento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addComponent(jBtnAgregarDetalle)
-                .addGap(15, 15, 15))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTablePedido.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null, null},
                 {null, null, null, null, null, null, null},
@@ -276,17 +285,9 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
                 "Id Pedido", "Proveedor", "Fecha pedido", "Fecha de entrega", "Fecha de envio", "Forma de envio", "Cargo"
             }
         ));
-        jScrollPane2.setViewportView(jTable1);
+        jScrollPane2.setViewportView(jTablePedido);
 
         jMenu1.setText("Archivo");
-
-        jMenuImportar.setText("Importar csv");
-        jMenuImportar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuImportarActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuImportar);
 
         jMenuPrincipal.setText("Menu Principal");
         jMenuPrincipal.addActionListener(new java.awt.event.ActionListener() {
@@ -298,6 +299,26 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu1);
 
+        jMenu2.setText("Importar");
+
+        jMenuPedidos.setText("Importar Pedidos");
+        jMenuPedidos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuPedidosActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuPedidos);
+
+        jMenuImportar.setText("Importar Detalle de Pedidos");
+        jMenuImportar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuImportarActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuImportar);
+
+        jMenuBar1.add(jMenu2);
+
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -305,9 +326,8 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
+                .addContainerGap(18, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jBtnGuardar)
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
@@ -333,31 +353,33 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextFechEntrega, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextFechEntrega, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 676, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(8, 8, 8))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 642, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(19, 19, 19)))
+                        .addGap(29, 29, 29)))
                 .addComponent(jScrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
                         .addComponent(jScrollBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 605, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(24, 24, 24)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(24, 24, 24)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(jLabel2)
                                     .addComponent(jTextId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -386,15 +408,15 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
                                     .addComponent(jLabel20)
                                     .addComponent(jTextCargo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(26, 26, 26)
-                                .addComponent(jBtnGuardar)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
-                                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(14, 14, 14))
+                                .addComponent(jBtnGuardar))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(41, 41, 41))))))
+                                .addGap(44, 44, 44)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 280, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
 
         pack();
@@ -418,18 +440,22 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
     jTextDetId.setText(jTextId.getText());
     }//GEN-LAST:event_jBtnGuardarActionPerformed
 
+    private void jMenuImportarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuImportarActionPerformed
+        cargarDetallePedidos();
+    
+    }//GEN-LAST:event_jMenuImportarActionPerformed
+
     private void jBtnAgregarDetalleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnAgregarDetalleActionPerformed
         tablaDetalles.addRow(new Object[]{jTextDetId.getText(),
-                                       jTextDetProducto.getText(),
-                                       jTextDetCantidad.getText(),
-                                       jTextDetPrecio.getText(),
-                                       jTextDetDescuento.getText()});
+            jTextDetProducto.getText(),
+            jTextDetCantidad.getText(),
+            jTextDetPrecio.getText(),
+            jTextDetDescuento.getText()});
     }//GEN-LAST:event_jBtnAgregarDetalleActionPerformed
 
-    private void jMenuImportarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuImportarActionPerformed
-    cargarRegistroPedidos();        
-        
-    }//GEN-LAST:event_jMenuImportarActionPerformed
+    private void jMenuPedidosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuPedidosActionPerformed
+        cargarDatosPedidos();
+    }//GEN-LAST:event_jMenuPedidosActionPerformed
 
     /**
      * @param args the command line arguments
@@ -484,17 +510,19 @@ public class Registro_de_Pedidos extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuImportar;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuPedidos;
     private javax.swing.JMenuItem jMenuPrincipal;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollBar jScrollBar1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTable jTableDetalPedido;
+    private javax.swing.JTable jTablePedido;
     private javax.swing.JTextField jTextCargo;
     private javax.swing.JTextField jTextDetCantidad;
     private javax.swing.JTextField jTextDetDescuento;
